7cbcb5b9be2a1a5978f58226bdebe993ae1dc708
Casey Townsend <casey.townsend@gmail.com>
2019-04-16 22:55:59 -0500

    Change .sass file extension to .scss to handle formatting

--- client/src/app/supplier/supplier.component.ts
+++ client/src/app/supplier/supplier.component.ts
@@ -5,7 +5,7 @@ import { Supplier } from '../supplier';
 @Component({
   selector: 'app-supplier',
   templateUrl: './supplier.component.html',
-  styleUrls: ['./supplier.component.sass']
+  styleUrls: ['./supplier.component.scss']
 })
 export class SupplierComponent implements OnInit {
   displayedColumns: string[] = ['supplierId', 'companyName', 'contactName'];
