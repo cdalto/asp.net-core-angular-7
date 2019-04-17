5c3ceec4e10eb8ff7f3173f12fefe4b7eefe1991
Casey Townsend <casey.townsend@gmail.com>
2019-04-16 22:24:59 -0500

    Apply steps 9 - 11

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { SupplierDetailComponent } from './supplier-detail/supplier-detail.component';
+import { SupplierAddComponent } from './supplier-add/supplier-add.component';
+import { SupplierEditComponent } from './supplier-edit/supplier-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SupplierComponent,
+    SupplierDetailComponent,
+    SupplierAddComponent,
+    SupplierEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
